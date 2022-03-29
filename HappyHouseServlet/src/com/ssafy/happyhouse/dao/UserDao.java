c45b2196f4be65a077193becf3b0ce25a9a210de
kimjuwon <sa6812000@naver.com>
2022-03-29 17:27:26 +0900

    DBUtil

--- HappyHouseServlet/src/com/ssafy/happyhouse/dao/UserDao.java
+++ HappyHouseServlet/src/com/ssafy/happyhouse/dao/UserDao.java
@@ -1,13 +1,14 @@
 package com.ssafy.happyhouse.dao;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import com.ssafy.happyhouse.dto.User;
 
 public interface UserDao {
-	User save(User user);      // 회원정보 저장(가입)
-	User findById(String id); // id 회원정보 조회
-	List<User> findALL();		//모든 회원정보 조회
-	int UpdateUser(User updateParam); // 회원정보 수정
-	int deleteUser(String id); // 회원정보 삭제(탈퇴)
+	User save(User user) throws SQLException;      // 회원정보 저장(가입)
+	User findById(String id) throws SQLException; // id 회원정보 조회
+	List<User> findALL() throws SQLException;		//모든 회원정보 조회
+	int UpdateUser(User updateParam) throws SQLException; // 회원정보 수정
+	int deleteUser(String id) throws SQLException; // 회원정보 삭제(탈퇴)
 }
